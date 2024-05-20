975788a3789a312633b4b9fe7a019cd64d225be7
Pan <2452996084@qq.com>
2024-05-20 11:25:17 +0800

    新增文件：Calculate model parameters.py

---
+++ Calculate model parameters.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   Calculate model parameters.py
+@Time    :   2024/05/20 11:25:10
+@Author  :   pan binghong 
+@Email   :   19909442097@163.com
+@description   :   
+'''
